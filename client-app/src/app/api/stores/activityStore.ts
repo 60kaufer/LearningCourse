abc694ac62138ca89f320aa3ece90cedc4d68096
Kauf <60kaufer@gmail.com>
2023-06-05 10:26:43 +0200

    end of section 8

--- client-app/src/app/api/stores/activityStore.ts
+++ client-app/src/app/api/stores/activityStore.ts
@@ -8,7 +8,7 @@ export default class ActivityStore {
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
 
     constructor() {
@@ -21,11 +21,11 @@ export default class ActivityStore {
     }
 
     loadActivities = async () => {
+        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activityRegistry.set(activity.id, activity);
+                this.setActvity(activity)
                 })
                 this.setLoadingInitial(false);
         } catch (error) {
@@ -34,26 +34,41 @@ export default class ActivityStore {
             
         }
     }
+
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if (activity) {
+            this.selectedActivity = activity;
+            return activity;
+        }
+        else {
+            this.setLoadingInitial (true);
+            try {
+                activity = await agent.Activities.details(id);
+                this.setActvity(activity);
+                runInAction(() => this.selectedActivity = activity);
+                this.setLoadingInitial(false);
+                return activity;
+
+            } catch (error) {
+                console.log(error)
+                this.setLoadingInitial(false)
+            }
+        }
+    }
+
+    private setActvity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id, activity);
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
     
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
-    }
-   
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id)
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined
-    }
-
-    openForm = (id?: string) => {
-        id? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
     }
 
     createActivity = async (activity: Activity) => {
@@ -103,7 +118,6 @@ export default class ActivityStore {
             await agent.Activities.delete(id);
             runInAction(() => {
                 this.activityRegistry.delete(id);
-                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
             })
             
--- client-app/src/features/activities/dashboard/details/ActivityDetails.tsx
+++ client-app/src/features/activities/dashboard/details/ActivityDetails.tsx
@@ -1,13 +1,21 @@
 import { Button, Card, Icon, Image } from "semantic-ui-react";
 import { useStore } from "../../../../app/api/stores/store";
 import LoadingComponent from "../../../../app/layout/loadingComponents";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { observer } from "mobx-react-lite";
 
-export default function ActivityDetails() {
+export default observer (function ActivityDetails() {
 
   const {activityStore} = useStore();
-  const {selectedActivity: activity, cancelSelectedActivity} = activityStore;
+  const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+  const {id} = useParams();
 
-  if (!activity) return <LoadingComponent />;
+  useEffect(() => {
+    if (id) loadActivity(id);
+  }, [id, loadActivity])
+
+  if (loadingInitial || !activity) return <LoadingComponent />;
 
     return (
         <Card fluid>
@@ -23,11 +31,11 @@ export default function ActivityDetails() {
     </Card.Content>
     <Card.Content extra>
       <Button.Group widths='2'>
-        <Button onClick={() => activityStore.openForm(activity.id)} basic color="blue" content='Edit' />
-        <Button onClick={cancelSelectedActivity} basic color="grey" content='Cancel' />
+        <Button as={Link} to={`/manage/${activity.id}`} basic color="blue" content='Edit' />
+        <Button as={Link} to='/activities' basic color="grey" content='Cancel' />
       </Button.Group>
     </Card.Content>
   </Card>
     )
 
-}
+})
