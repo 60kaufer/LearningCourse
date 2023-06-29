2235459fcdc83b7fcadd59cb85d6840f29cae1ae
Kauf <60kaufer@gmail.com>
2023-06-29 14:27:48 +0200

    End of section 12

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,11 +1,11 @@
 //Persistence basically defines how to interact with the model using C# to create SQL queries through LINQ
-
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
